d5f74e77fe5f212caaf55232fc160f4eadc8ef84
Kadirov Kamoliddin <kadirovkamoliddin24@gmail.com>
2024-10-10 20:20:54 +0400

    Add all Files

---
+++ script.js
@@ -0,0 +1,33 @@
+/*this one for white*/ 
+function imgwhite(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+/*this one for black*/ 
+function imgblack(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+/*this one for red*/ 
+function imgred(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+/*this one for gary*/ 
+function imggary(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+/*this one for darkgray*/ 
+function imgdarkgray(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+/*And so on*/
+function imgblue(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+function imgdarkblue(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+function imgwhite(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
+function imgwhite(){
+    document.getElementById('imgwhite').src='iris(1).jpeg'
+}
